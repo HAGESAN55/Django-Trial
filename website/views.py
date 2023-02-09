a8d67cfaf64fdbed7779d179b260cf785329dcee
unknown <fadli55fadillah@gmail.com>
2023-02-09 14:21:34 +0700

    Django First Trial

---
+++ website/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+  return render(request, 'index.html')
+
+def beranda(request):
+  return render(request, 'beranda.html')
+
